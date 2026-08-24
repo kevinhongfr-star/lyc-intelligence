93f81fd662ceedfe60a246afd6285418a50a94d8
kevinhongfr-star <kevin.hong.fr@gmail.com>
2026-08-25 00:43:42 +0800

    cleanup: remove diagnostic test files from P0 investigation

--- api/edge-test.ts
+++
@@ -1,8 +0,0 @@
-export const config = { runtime: 'edge' };
-
-export default function handler(_req: Request) {
-  return new Response(JSON.stringify({ ok: true, message: 'edge test works' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
